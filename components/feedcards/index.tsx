e6ea68335cf01a13e700d2121276b48d94ab7f58
hritik kumar <hritik.7827@gmail.com>
2024-07-21 14:41:12 +0530

    Added basic homePage Layout

---
+++ components/feedcards/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa6";
+import { AiOutlineHeart } from "react-icons/ai";
+import Link from "next/link";
+import { RiShare2Line } from "react-icons/ri";
+import { PiBookmarkSimple } from "react-icons/pi";
+
+const FeedCard = () => {
+  return (
+    <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">👤</div>
+        <div className="col-span-11">
+          <h5>Hritik Shaw</h5>
+          <p>
+            this is my first tweet this is my first tweet this is my first
+            tweetthis is my first tweetthis is my first tweet this is my first
+            tweetthis is my first this is my first tweet this is my first tweet
+            this is my first tweetthis is my first tweet
+          </p>
+
+          <div className="flex justify-between mt-5 text-lg  p-2 w-[100%]">
+            <div>
+              <BiMessageRounded />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <AiOutlineHeart />
+            </div>
+            <div>
+              <PiBookmarkSimple />
+            </div>
+            <div>
+              <RiShare2Line />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedCard;
